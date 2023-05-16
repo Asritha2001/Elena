cec1e0d40bd2b8278d79c27ddff31482bdcee027
Dilara Tekinoglu <dilaratekinoglu@Dilaras-MacBook-Air.local>
2023-05-15 21:19:17 -0400

    Change url

--- Elena-Backend/src/main/java/com/umass/elena/GoogleMapsController.java
+++ Elena-Backend/src/main/java/com/umass/elena/GoogleMapsController.java
@@ -3,15 +3,12 @@ package com.umass.elena;
 import com.google.maps.errors.ApiException;
 import com.google.maps.model.DirectionsRoute;
 import java.io.IOException;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-/**
- * @author bahadirmemis
- */
+import org.springframework.web.bind.annotation.*;
+
+
 @RestController
+@RequestMapping("google-maps")
 public class GoogleMapsController {
 
   private final GoogleMapsService googleMapsService;
@@ -21,8 +18,8 @@ public class GoogleMapsController {
   }
 
   @CrossOrigin(origins = "http://localhost:3000")
-  @GetMapping("/test")
-  public DirectionsRoute[] getMailSendRequestDto(@RequestParam String source,
+  @GetMapping("/routes")
+  public DirectionsRoute[] getRoutes(@RequestParam String source,
                                                  @RequestParam String destination,
                                                  @RequestParam Integer x) throws IOException, InterruptedException, ApiException {
 
@@ -31,23 +28,5 @@ public class GoogleMapsController {
     System.out.println(5);
 
     return routes;
-
-    //GeoApiContext context = new GeoApiContext.Builder()
-    //    .apiKey("AIzaSyA-UCoOovnOluxqPTNjCLQOPndIUa0Wf5A")
-    //    .build();
-    //GeocodingResult[] results = GeocodingApi.geocode(context,
-    //                                                 "1600 Amphitheatre Parkway Mountain View, CA 94043").await();
-    //Gson gson = new GsonBuilder().setPrettyPrinting().create();
-    //System.out.println(gson.toJson(results[0].addressComponents));
-    //
-    //// Invoke .shutdown() after your application is done making requests
-    //context.shutdown();
-
-    //String url = "";
-    //RestTemplate restTemplate = new RestTemplate();
-    //
-    //ResponseEntity<MailSendRequestDto> responseEntity = restTemplate.getForEntity(url, MailSendRequestDto.class);
-    //
-    //return responseEntity.getBody();
   }
 }
